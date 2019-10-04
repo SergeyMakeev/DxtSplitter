6605f426fcfad718d9542546bdb7d7fbd536237e
Sergey Makeev <sergeymakeev@hotmail.com>
2019-10-04 09:00:23 -0700

    DXT decompresor and PSNR analyzer

--- src/main.cpp
+++ src/main.cpp
@@ -76,6 +76,11 @@ struct RGB
 };
 
 
+struct BlockDxt1Uncompressed
+{
+    RGB pixels[16];
+};
+
 static RGB unpackColor565(uint16_t color)
 {
     uint32_t b = (color & 0x1f) << 3;
@@ -99,6 +104,79 @@ static RGB unpackColor565(uint16_t color)
 }
 
 
+static BlockDxt1Uncompressed decompressDxt1Block(const BlockDxt1& block)
+{
+    // DXT1 with alpha doesn't supported !
+    BlockDxt1Uncompressed result;
+
+    RGB palette[4];
+
+    // endpoints
+    palette[0] = unpackColor565(block.endPointA);
+    palette[1] = unpackColor565(block.endPointB);
+
+    // midpoints
+    palette[2].r = ((2 * palette[0].r + palette[1].r) / 3);
+    palette[2].g = ((2 * palette[0].g + palette[1].g) / 3);
+    palette[2].b = ((2 * palette[0].b + palette[1].b) / 3);
+
+    palette[3].r = ((palette[0].r + 2 * palette[1].r) / 3);
+    palette[3].g = ((palette[0].g + 2 * palette[1].g) / 3);
+    palette[3].b = ((palette[0].b + 2 * palette[1].b) / 3);
+
+    uint32_t ind = block.indices;
+    for (uint32_t i = 0; i < 16; i++)
+    {
+        uint32_t currentIndice = (ind & 0x3);
+        result.pixels[i] = palette[currentIndice];
+        ind = ind >> 2;
+    }
+
+    return result;
+}
+
+
+static double compareBlocks(const BlockDxt1Uncompressed& a, const BlockDxt1Uncompressed& b)
+{
+    double mseR = 0.0;
+    double mseG = 0.0;
+    double mseB = 0.0;
+
+    for (int i = 0; i < 16; i++)
+    {
+        double errR = (a.pixels[i].r - b.pixels[i].r);
+        double errG = (a.pixels[i].g - b.pixels[i].g);
+        double errB = (a.pixels[i].b - b.pixels[i].b);
+
+        mseR += errR * errR;
+        mseG += errG * errG;
+        mseB += errB * errB;
+    }
+
+    mseR = mseR / 16.0;
+    mseG = mseG / 16.0;
+    mseB = mseB / 16.0;
+
+    const double kPixelMaxValue = 255.0;
+
+    double psnrR = DBL_MAX;
+    double psnrG = DBL_MAX;
+    double psnrB = DBL_MAX;
+
+    if (mseR > 0.0)
+        psnrR = 10.0 * log10(kPixelMaxValue * kPixelMaxValue / mseR);
+
+    if (mseG > 0.0)
+        psnrG = 10.0 * log10(kPixelMaxValue * kPixelMaxValue / mseG);
+
+    if (mseB > 0.0)
+        psnrB = 10.0 * log10(kPixelMaxValue * kPixelMaxValue / mseB);
+
+    double psnr = std::min(std::min(psnrR, psnrG), psnrB);
+
+    return psnr;
+}
+
 static void saveToTga(const char* fileName, const std::vector<RGB>& pixels, uint32_t width, uint32_t height)
 {
     TGA_HEADER header;
@@ -116,6 +194,131 @@ static void saveToTga(const char* fileName, const std::vector<RGB>& pixels, uint
     fwrite(&pixels[0], 1, pixels.size() * sizeof(RGB), file);
 
     fclose(file);
+}
+
+
+static void analyzeImage(const std::vector<uint8_t>& mipData, uint32_t width, uint32_t height)
+{
+    uint32_t blockCountW = width / 4;
+    uint32_t blockCountH = height / 4;
+
+    uint32_t blockLineStride = blockCountW * sizeof(BlockDxt1);
+
+    uint32_t blockCount = blockCountW * blockCountH;
+
+    std::vector<BlockDxt1Uncompressed> decompressedBlocks(blockCount);
+
+    // decompress all DXT blocks
+    for (uint32_t y = 0; y < blockCountH; y++)
+    {
+        for (uint32_t x = 0; x < blockCountW; x++)
+        {
+            uint32_t blockOffset = (y * blockLineStride) + (x * sizeof(BlockDxt1));
+
+            BlockDxt1 block;
+            memcpy(&block, &mipData[blockOffset], sizeof(BlockDxt1));
+
+            decompressedBlocks[y * blockCountW + x] = decompressDxt1Block(block);
+        }
+    }
+
+    printf("Number of blocks: %d\n", (int)decompressedBlocks.size());
+
+    // PSNR db histogram
+    std::array<uint32_t, 52> histogram;
+    histogram.fill(0);
+
+    printf("block;psnr min; psnr max;px;py;spx;spy;\n");
+
+    // Analyze blocks similarity between blocks
+    for (size_t i = 0; i < decompressedBlocks.size(); i++)
+    {
+        double maxBlockPsnr = -DBL_MAX;
+        double minBlockPsnr = DBL_MAX;
+        uint32_t similarBlockIndex = i;
+
+        for (size_t j = 0; j < decompressedBlocks.size(); j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+
+            double psnr = compareBlocks(decompressedBlocks[i], decompressedBlocks[j]);
+
+            if (psnr > maxBlockPsnr)
+            {
+                maxBlockPsnr = psnr;
+                similarBlockIndex = j;
+            }
+
+            minBlockPsnr = std::min(minBlockPsnr, psnr);
+        }
+
+        uint32_t x = (i % blockCountW) * 4;
+        uint32_t y = (i / blockCountW) * 4;
+
+        uint32_t sx = (similarBlockIndex % blockCountW) * 4;
+        uint32_t sy = (similarBlockIndex / blockCountW) * 4;
+
+        double tmp = round(maxBlockPsnr);
+        if (tmp < 0.0)
+            tmp = 0;
+
+        if (tmp > 51.0)
+            tmp = 51.0;
+
+        int bucketIndex = (int)tmp;
+        histogram[bucketIndex]++;
+
+        printf("%d;%f;%f;%d;%d;%d;%d\n", i, minBlockPsnr, maxBlockPsnr, x, y, sx, sy);
+    }
+
+
+    // print histogram
+    printf("dB;count\n");
+    for (size_t i = 0; i < histogram.size(); i++)
+    {
+        printf("%d;%d\n", i, histogram[i]);
+    }
+
+}
+
+
+static void decompressAndSave(const std::vector<uint8_t>& mipData, uint32_t width, uint32_t height, const char* fileName)
+{
+    uint32_t blockCountW = width / 4;
+    uint32_t blockCountH = height / 4;
+
+    uint32_t blockLineStride = blockCountW * sizeof(BlockDxt1);
+
+    std::vector<RGB> decompressed(width * height);
+
+    for (uint32_t y = 0; y < blockCountH; y++)
+    {
+        for (uint32_t x = 0; x < blockCountW; x++)
+        {
+            uint32_t blockOffset = (y * blockLineStride) + (x * sizeof(BlockDxt1));
+
+            BlockDxt1 block;
+            memcpy(&block, &mipData[blockOffset], sizeof(BlockDxt1));
+
+            BlockDxt1Uncompressed blockUncompressed = decompressDxt1Block(block);
+
+            // copy current block to the final image
+            for (uint32_t oy = 0; oy < 4; oy++)
+            {
+                for (uint32_t ox = 0; ox < 4; ox++)
+                {
+                    uint32_t pixelOffset = (y * 4 + oy) * width + (x * 4 + ox);
+                    decompressed[pixelOffset] = blockUncompressed.pixels[oy * 4 + ox];
+                }
+            }
+        }
+    }
+
+    printf("Saving decompressed image to '%s'\n", fileName);
+    saveToTga(fileName, decompressed, width, height);
 }
 
 static void splitAndSave(const std::vector<uint8_t>& mipData, uint32_t width, uint32_t height, const char* colorNameA, const char* colorNameB, const char* indicesName)
@@ -150,7 +353,7 @@ static void splitAndSave(const std::vector<uint8_t>& mipData, uint32_t width, ui
             BlockDxt1 block;
             memcpy(&block, &mipData[blockOffset], sizeof(BlockDxt1));
 
-            // decode end-points
+            // decode endpoints
             uint32_t offset = y * blockCountW + x;
             endPointsA[offset] = unpackColor565(block.endPointA);
             endPointsB[offset] = unpackColor565(block.endPointB);
@@ -222,8 +425,8 @@ static void splitAndSave(const std::vector<uint8_t>& mipData, uint32_t width, ui
     uint32_t numbBytesForIndices = blockCountW * blockCountH * 4;
 
     printf("\n");
-    printf("End-points-A data size = %d bytes (25%%)\n", numbBytesForEndPoints);
-    printf("End-points-B data size = %d bytes (25%%)\n", numbBytesForEndPoints);
+    printf("Endpoints-A data size = %d bytes (25%%)\n", numbBytesForEndPoints);
+    printf("Endpoints-B data size = %d bytes (25%%)\n", numbBytesForEndPoints);
     printf("Indices data size = %d bytes (50%%)\n", numbBytesForIndices);
     printf("Number of blocks: %d\n", blockCount);
     uint32_t sharedColorBlocks = blockCount - (uint32_t)uniqueColors.size();
@@ -232,10 +435,10 @@ static void splitAndSave(const std::vector<uint8_t>& mipData, uint32_t width, ui
     printf("Shared indices blocks: %d (%3.2f%%)\n", sharedIndicesBlocks, 100.0f * sharedIndicesBlocks / (float)blockCount);
     printf("\n");
 
-    printf("Saving end-points-A to '%s'\n", colorNameA);
+    printf("Saving endpoints-A to '%s'\n", colorNameA);
     saveToTga(colorNameA, endPointsA, blockCountW, blockCountH);
 
-    printf("Saving end-points-B to '%s'\n", colorNameB);
+    printf("Saving endpoints-B to '%s'\n", colorNameB);
     saveToTga(colorNameB, endPointsB, blockCountW, blockCountH);
 
     printf("Saving indices to '%s'\n", indicesName);
@@ -346,6 +549,10 @@ int main()
     }
 
     fclose(file);
+
+    //analyzeImage(data[0], header.width, header.height);
+
+    //decompressAndSave(data[0], header.width, header.height, "mip0_uncompressed_image.tga");
 
     splitAndSave(data[0], header.width, header.height, "mip0_colorA.tga", "mip0_colorB.tga", "mip0_indices.tga");
     //splitAndSave(data[1], (header.width >> 1), (header.height >> 1), "mip1_colorA.tga", "mip1_colorB.tga", "mip1_indices.tga");
